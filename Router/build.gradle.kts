c2fbcb24d30dbbc18ec1575543c02cb1721fc4d2
Michael J. May <mjmay-kinneret@users.noreply.github.com>
2024-06-03 17:04:12 +0300

    Project setup

---
+++ Router/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+    application
+}
+
+group = "il.ac.kinneret.subnet"
+version = "5784"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("il.ac.kinneret.networks.subnet.Router")
+}
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "il.ac.kinneret.networks.subnet.Router"
+    }
+}
---
+++ Router/src/main/java/il/ac/kinneret/networks/subnet/Router.java
@@ -0,0 +1,7 @@
+package il.ac.kinneret.networks.subnet;
+
+public class Router {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
