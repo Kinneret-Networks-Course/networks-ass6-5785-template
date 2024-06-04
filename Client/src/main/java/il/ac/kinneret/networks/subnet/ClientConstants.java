3a4f0bedbda64d2286e65ffb3ea8b2a88da3923c
Michael J. May <mjmay-kinneret@users.noreply.github.com>
2024-06-04 15:18:51 +0300

    added constant strings

---
+++ Client/src/main/java/il/ac/kinneret/networks/subnet/ClientConstants.java
@@ -0,0 +1,17 @@
+package il.ac.kinneret.networks.subnet;
+
+public class ClientConstants {
+    public static final String PORT = "port";
+    public static final String DEST = "dest";
+    public static final String ADDRESSES = "addresses";
+    public static final String NOT_FOUND = "NotFound";
+
+    public static final String ERROR_ILLEGAL_IP_ADDRESS = "Error: Illegal IP address: %s%n";
+    public static final String ERROR_ILLEGAL_PORT_NUMBER = "Error: Illegal port number: %s%n";
+    public static final String CONNECTED_TO_SERVER = "Connected to server%n";
+    public static final String NOT_FOUND_OUTPUT = "%s not found%n";
+    public static final String FOUND_OUTPUT = "%s -> %s%n";
+    public static final String ERROR_COMMUNICATING = "Error communicating: %s%n";
+    public static final String USAGE_MESSAGE = "Usage: Client -dest=ip -port=p -addresses=adds%n";
+    public static final String ERROR_MISSING_PARAMETER = "Error missing parameter%n";
+}
---
+++ Router/src/main/java/il/ac/kinneret/networks/subnet/RouterConstants.java
@@ -0,0 +1,27 @@
+package il.ac.kinneret.networks.subnet;
+
+public class RouterConstants {
+    public static final String PORT = "port";
+    public static final String IP = "ip";
+    public static final String TABLE = "table";
+    public static final String NOT_FOUND = "NotFound";
+
+    public static final String MISSING_PARAMETER = "Missing parameter%n";
+    public static final String ERROR_ILLEGAL_LINE_FORMAT = "Error: Illegal line format: %n";
+    public static final String ERROR_ILLEGALLY_FORMATTED_LINE = "Error: Illegally formatted line: %s: %s%n";
+    public static final String READ_ENTRIES = "Read %s entries%n";
+    public static final String PRESS_Q_TO_QUIT = "Press q to quit%n";
+    public static final String STARTED_LISTENING_ON = "Started listening on %s:%s%n";
+    public static final String QUITTING = "Quitting%n";
+    public static final String RECEIVED_ILLEGAL_ADDRESS = "Received illegal address: %s%n";
+    public static final String ILLEGAL_ADDRESS = "Illegal address: %s%n";
+    public static final String ERROR_COMMUNICATING_WITH_CLIENT = "Error communicating with client: %s%n";
+    public static final String CLOSED_CONNECTION = "Closed connection from: %s%n";
+    public static final String ERROR_PARSING_PORT = "Error parsing port: %s%n";
+    public static final String ERROR_WITH_IP_ADDRESS = "Error with IP address: %s%n";
+    public static final String ERROR_LISTENING = "Error listening: %s%n";
+    public static final String ERROR_READING_TABLE_FILE = "Error reading table file: %s%n";
+    public static final String USAGE_MESSAGE = "Usage: SubnetServer -table=t -ip=ip -port=p%n";
+    public static final String RECEIVED_CONNECTION_FROM = "Received connection from %s%n";
+    public static final String STOPPED_LISTENING = "Stopped listening%n";
+}
