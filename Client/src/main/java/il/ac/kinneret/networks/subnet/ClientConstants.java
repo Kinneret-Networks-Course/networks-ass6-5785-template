f571dcdcf3a0946779afc99c348611d1942a630e
Michael J. May <mjmay-kinneret@users.noreply.github.com>
2025-01-16 16:01:00 +0200

    updated test results

--- Client/src/main/java/il/ac/kinneret/networks/subnet/ClientConstants.java
+++ Client/src/main/java/il/ac/kinneret/networks/subnet/ClientConstants.java
@@ -8,10 +8,10 @@ public class ClientConstants {
 
     public static final String ERROR_ILLEGAL_IP_ADDRESS = "Error: Illegal IP address: %s%n";
     public static final String ERROR_ILLEGAL_PORT_NUMBER = "Error: Illegal port number: %s%n";
-    public static final String CONNECTED_TO_SERVER = "Connected to router%n";
+    public static final String CONNECTED_TO_ROUTER = "Connected to router%n";
     public static final String NOT_FOUND_OUTPUT = "%s not found%n";
     public static final String FOUND_OUTPUT = "%s -> %s%n";
     public static final String ERROR_COMMUNICATING = "Error communicating: %s%n";
-    public static final String USAGE_MESSAGE = "Usage: Client-5784 -dest=ip -port=p -addresses=adds%n";
+    public static final String USAGE_MESSAGE = "Usage: Client-5785 -dest=ip -port=p -addresses=adds%n";
     public static final String ERROR_MISSING_PARAMETER = "Error missing parameter%n";
 }
